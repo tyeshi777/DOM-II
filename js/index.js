3a157e999a3e73ee67fab7f518f5401ce624c373
Yeshi <tyeshi777@yahoo.com>
2019-03-05 15:19:35 -0800

    second commit

--- js/index.js
+++ js/index.js
@@ -1,13 +1,31 @@
 // Your code goes here
+window.addEventListener("load", function() {
+  console.log(`your page is loaded`);
+});
+
+const navigation = document.querySelectorAll("a");
+console.log(navigation);
+navigation.forEach(item => {
+  item.addEventListener("mouseenter", function(event) {
+    event.target.style.color = "red";
+    event.target.style.fontSize = "20px";
+    event.preventDefault();
+  });
+});
+
 const logoHeading = document.querySelector(".logo-heading");
 logoHeading.addEventListener("mouseover", function(event) {
   event.target.style.fontSize = "60px";
   event.target.style.color = "red";
 });
 
-const btn = document.querySelector(".btn");
-btn.addEventListener("click", function(event) {
-  event.target.style.borderWidth = "thick";
+const btn = document.querySelectorAll(".btn");
+btn.forEach(item => {
+  item.addEventListener("click", function(event) {
+    event.target.style.borderWidth = "thick";
+    event.target.style.background = "crimson";
+    event.preventDefault();
+  });
 });
 
 const funBus = document.querySelector(".fun-bus");
@@ -15,3 +33,15 @@ funBus.addEventListener("dblclick", function(event) {
   event.target.style.width = "1000px";
   event.target.style.height = "500px";
 });
+
+// let home = document.querySelector(".home");
+window.addEventListener("keydown", function(event) {
+  console.log(`you pressed down this key ${event.key}`);
+});
+
+window.addEventListener("resize", display);
+function display() {
+  const x = document.querySelector(".text-content");
+  x.style.background = "purple";
+  x.style.color = "white";
+}

5876d15692b3c1682e928a2f362a310a55cd8e96
Yeshi <tyeshi777@yahoo.com>
2019-03-05 12:52:02 -0800

    first commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+const logoHeading = document.querySelector(".logo-heading");
+logoHeading.addEventListener("mouseover", function(event) {
+  event.target.style.fontSize = "60px";
+  event.target.style.color = "red";
+});
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", function(event) {
+  event.target.style.borderWidth = "thick";
+});
+
+const funBus = document.querySelector(".fun-bus");
+funBus.addEventListener("dblclick", function(event) {
+  event.target.style.width = "1000px";
+  event.target.style.height = "500px";
+});
